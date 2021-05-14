7141246a22b680c4aa49ca4e9a1bc6cf77d74740
hL32 <75103969+hL32@users.noreply.github.com>
2021-05-14 08:47:48 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Quản Lý website bán hoa quả</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+	<!-- Popper JS -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<ul class="nav nav-tabs">
+		<li class="nav-item">
+				<a class="nav-link" href="#">Quản Lý Danh Mục </a>
+		</li>
+	</ul>
+	<h2 class="text-center">Quản Lý Danh Mục </h2>
+</body>
+</html>
